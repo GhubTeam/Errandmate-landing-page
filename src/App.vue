8f19c3509d0f6b1d825df40366ca32cc4731fd54
Ojingwa Daniel <ojingwadanny@gmail.com>
2026-09-22 21:51:05 +0100

    added some images

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import pickup from "./images/img-4.jpg";
 import delivery from "./images/img-5.jpg";
 import commerce from "./images/img-6.jpg";
 import rider from "./images/img-7.jpg";
+import rider2 from "./images/img-9.jpg";
 import tracking from "./images/img-8.jpg";
 const menuOpen = ref(false)
 const logo = 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/errandmate%20logo-vJdc90wFL0P4W3bGmSjrcuWdmnw5Dd.png'
@@ -19,6 +20,7 @@ const images = {
   delivery: delivery,
   commerce : commerce,
   rider:rider,
+  rider2:rider2,
   tracking : tracking
 }
 const navItems = ['How it works', 'Customers', 'Runners', 'Merchants']
@@ -29,7 +31,7 @@ const roles = [
 ]
 const steps = [
   { number: '01', title: 'Create Your Errand', text: 'Choose what you need — shopping, pickup, delivery, merchant order or a custom errand.', image: images.customer, icon: '＋' },
-  { number: '02', title: 'Get Matched', text: 'ErrandMate connects your task with an available verified runner.', image: images.runner, icon: '⌁' },
+  { number: '02', title: 'Get Matched', text: 'ErrandMate connects your task with an available verified runner.', image: images.rider2, icon: '⌁' },
   { number: '03', title: 'Track Everything', text: 'Follow your runner and receive updates as your errand moves from pickup to completion.', image: images.pickup, icon: '◎' },
   { number: '04', title: 'Done.', text: 'Your errand is completed. Review the experience and get back to your day.', image: images.delivery, icon: '✓' },
 ]
